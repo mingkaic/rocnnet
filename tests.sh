73b25076ff8fa11d8c69982b03618812f3968793
mingkaic <mingkaichen2009@gmail.com>
2017-05-15 10:50:30 -0700

    ignore python wrapper in coverage

--- tests.sh
+++ tests.sh
@@ -72,6 +72,6 @@ pushd ${BUILDDIR}
 lcov --version
 gcov --version
 lcov --directory . --gcov-tool gcov-6 --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '**/gtest*' '**/tests/*' '/usr/*' --output-file coverage.info # filter out system and test code
+lcov --remove coverage.info '**/gtest*' '**/tests/*' '**/build/*' '/usr/*' --output-file coverage.info # filter out system and test code
 lcov --list coverage.info # debug < see coverage here
 on_travis coveralls-lcov --repo-token ${COVERALLS_TOKEN} coverage.info # uploads to coveralls
