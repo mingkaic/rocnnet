c4bb1355680eaa24a5d589b5e9767e88e1d72add
mkaichen <mingkaichen2009@gmail.com>
2017-07-13 08:12:28 +0000

    account for empty version strings

--- setup.sh
+++ setup.sh
@@ -71,8 +71,7 @@ function meets_version() {
     REQ_VER=$1;
     CUR_VER=$2;
     CMD=${@:3:$#-2};
-    echo "YOOO $CMD"
-    if [ "$(printf "$REQ_VER\n$CUR_VER" | sort -V | head -n1)" == "$CUR_VER" ] && [ "$CUR_VER" != "$REQ_VER" ];
+    if [ -z $CUR_VER ] || [ "$(printf "$REQ_VER\n$CUR_VER" | sort -V | head -n1)" == "$CUR_VER" ] && [ "$CUR_VER" != "$REQ_VER" ];
     then
         exec_cmd "$CMD";
     else
@@ -127,23 +126,23 @@ easy_install pip;
 
 REQ_GCC_VER="6.0.0";
 CUR_GCC_VER="$(gcc -dumpversion)";
-meets_version $REQ_GCC_VER $CUR_GCC_VER "update_install gcc-6 && rm /usr/bin/gcc && mv /usr/bin/gcc-6 /usr/bin/gcc";
+meets_version "$REQ_GCC_VER" "$CUR_GCC_VER" "update_install gcc-6 && rm /usr/bin/gcc && mv /usr/bin/gcc-6 /usr/bin/gcc";
 
 REQ_GPP_VER="6.0.0";
 CUR_GPP_VER="$(g++ -dumpversion)";
-meets_version $REQ_GPP_VER $CUR_GPP_VER "update_install g++-6 && rm /usr/bin/g++ && mv /usr/bin/g++-6 /usr/bin/g++";
+meets_version "$REQ_GPP_VER" "$CUR_GPP_VER" "update_install g++-6 && rm /usr/bin/g++ && mv /usr/bin/g++-6 /usr/bin/g++";
 
 # ===== Install Cmake =====
 
 REQ_CMAKE_VER="3.0.0";
 CUR_CMAKE_VER="$(cmake --version)";
-meets_version $REQ_CMAKE_VER $CUR_CMAKE_VER "update_install cmake";
+meets_version "$REQ_CMAKE_VER" "$CUR_CMAKE_VER" "update_install cmake";
 
 # ===== Build dependencies =====
 
 # download protobuf3 and cache if necessary
 incache $PROTODIR "download_cfg_build https://github.com/google/protobuf/releases/download/v3.2.0/protobuf-cpp-3.2.0.tar.gz protobuf-cpp-3.2.0.tar.gz $PROTODIR";
-exec_cmd "pushd $CACHEDIR/$PROTODIR && make install && ldconfig && popd"
+exec_cmd "pushd $CACHEDIR/$PROTODIR && make install && ldconfig && popd" 
 
 # install swig
 exec_cmd "update_install swig";
