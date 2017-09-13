5cfb6a454294d169a56cf6dbd23e7a3bfafd89d2
mingkaic <mingkaichen2009@gmail.com>
2017-09-12 19:29:02 -0700

    share everything before lcov in container

--- app/scripts/test/tests.sh
+++ app/scripts/test/tests.sh
@@ -68,17 +68,18 @@ echo "python dq_demo complete"
 #echo "rbm_demo complete"
 
 # ===== Coverage Analysis ======
-lcov --version
-gcov --version
-lcov --base-directory . --directory . --gcov-tool gcov-6 --capture --output-file coverage.info # capture coverage info
-lcov --remove coverage.info '**/gtest*' '**/tests/*' '**/ipython/*' '**/bin/*' '**/build/*' '/usr/include/*' --output-file coverage.info # filter out system and test code
-lcov --list coverage.info # debug < see coverage here
-
 if ! [ -z "$SHARED_DIR" ];
 then
     echo "copying over everything to shared"
     cp -R . $SHARED_DIR
 fi
+
+lcov --version
+gcov --version
+lcov --base-directory . --directory . --gcov-tool gcov-6 --capture --output-file coverage.info # capture coverage info
+# filter out system and test code
+lcov --remove coverage.info '**/gtest*' '**/tests/*' '**/ipython/*' '**/bin/*' '**/build/*' '/usr/include/*' --output-file coverage.info
+lcov --list coverage.info # debug < see coverage here
 
 echo "";
 echo "============ ROCNNET TEST SUCCESS============";
